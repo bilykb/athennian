642b5e18cf0a41b98c756d84328bc0257076ebd4
Brett Bilyk <brettbilyk@gmail.com>
2022-02-13 20:11:28 -0700

    Add tests for reciteLyrics(number)

--- test/testQuestion.test.js
+++ test/testQuestion.test.js
@@ -34,3 +34,48 @@ describe('getEnding()', function() {
     expect(songVerse).to.equal(expectedOutput);
   })
 });
+
+describe('reciteLyrics(number)', function() {
+
+  // wrapper necessary for recursive function testing
+  const wrapper = {
+    startingNumber: 5,
+    reciteLyrics: number => {
+
+      if (number === 0) return wrapper.getEnding();
+      
+      wrapper.getRefrain(number)
+      return wrapper.reciteLyrics(number - 1)
+    },
+    getEnding: () => {
+      return `no more bottles of beer on the wall\nno more bottles of beer\nGo to the store, buy some more, ${this.startingNumber} bottles of beer on the wall`
+    } ,
+    getRefrain: number => {
+      return `${number} bottles of beer on the wall\n${number} bottles of beer\ntake one down, pass it around ${number -1} bottles of beer on the wall`
+    }
+  };
+
+  it('is only called 6 times, if reciteLyrics(5)', function() {
+    const spy = sinon.spy(wrapper, 'reciteLyrics');
+ 
+    wrapper.reciteLyrics(5);   // sets number of beers
+    
+    expect(spy.callCount).to.equal(6); 
+  });
+
+  it('only calls on getEnding() once, if reciteLyrics(50)', () => {
+    const spy = sinon.spy(wrapper, 'getEnding');
+
+    wrapper.reciteLyrics(50)
+
+    expect(spy.callCount).to.equal(1);
+  });
+  
+  it('only calls on getRefrain 99 times, if reciteLyrics(99)', () => {
+    const spy = sinon.spy(wrapper, 'getRefrain');
+
+    wrapper.reciteLyrics(99)
+
+    expect(spy.callCount).to.equal(99)
+  });
+});
